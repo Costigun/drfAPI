9491050cbd08d92b90f7a2518a8578d7eb3563a3
Costigun <Costigun@github.com>
2018-10-23 23:32:34 +0600

    Some Tests

--- drf/rest/tests.py
+++ drf/rest/tests.py
@@ -1,10 +1,31 @@
 from django.test import TestCase
 from .models import Ezone
-# Create your tests here.
+from django.utils import timezone
+from django.urls import reverse
+
 class EzoneTestCase(TestCase):
     def setUp(self):
-        Ezone.objects.create(name="Like")
+        self.name = "Tests"
+        self.description = "tests for app"
+        self.category = "tested"
+        Ezone.objects.create(name=self.name,
+                             description=self.description,
+                             category=self.category
+                             )
 
-    def test_ezone_can_add(self):
-        like = Ezone.objects.get(name='lice')
-        self.assertEqual(like.speak(),'Like is good')
+
+    def test_max_length(self):
+        name = Ezone.objects.get(id=1)
+        max_length = name._meta.get.field('name').max_length
+        self.assertEqual(max_length,15)
+
+
+    def test_create_notes(self):
+        number = 5
+        for new_notes in range (number):
+            Ezone.objects.create(name="test" % new_notes,
+                                 description="lorem ipsum dolore" % new_notes,
+                                 category="categoryyyyyyyyyy" % new_notes
+                                 )
+
+
